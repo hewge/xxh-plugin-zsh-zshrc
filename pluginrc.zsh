863800499cdbf68e14794cf81431fc7370bac4e1
Johan Hägglund <johan.hagglund@glimworks.com>
2023-06-05 14:57:46 +0200

    Trying to set plugin variable.

--- pluginrc.zsh
+++ pluginrc.zsh
@@ -2,3 +2,6 @@
 # Put here your functions, environment variables, aliases and whatever you need.
 
 CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+plugins=(git vi-mode fzf mongocli gitignore kubectl kubectx z fd catimg colorize emoji fasd grunt gulp helm ripgrep rsync sudo)
+source $ZSH/oh-my-zsh.sh
